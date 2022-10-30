3206599b4957f5a2e988d47f654672ec5665331b
Rishabh Jain <rishabh.jain@corevaluetech.com>
2022-10-30 13:43:13 +0530

    Added solutions

--- src/com/dsa/Main.java
+++ src/com/dsa/Main.java
@@ -1,8 +1,11 @@
 package com.dsa;
 
-public class Main {
+import java.util.*;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main {
+    public static void main (String[] args) {
     }
-}
+
+};
+
+
---
+++ src/com/dsa/QuickSort.java
@@ -0,0 +1,15 @@
+package com.dsa;
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+    public static void main(String[] args) {
+    }
+    public static void swapElements(int[] arr , int x , int y) {
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+}
+
